b42fb4b8e663678eb865d3d3be78bd34a6a492cf
hoanbklucky <ngothanhhoan@gmail.com>
2024-08-06 10:59:16 -0400

    hw 1 2 3

---
+++ hw2_3.py
@@ -0,0 +1,10 @@
+# City Names: Write a function called city_country() that takes in the name of a capital and its country.
+# The function should return a string formatted like below if parameter capital is Santiago and parameter country is Chile
+# "Santiago is the capital of Chile"
+def city_country(capital, country):
+    return f'{capital} is the capital of {country}'
+
+if __name__ == "__main__":
+    capital = input("Enter a capital: ")
+    country = input("Enter a country: ")
+    print(city_country(capital, country))
